3899c6e578110fa54e5c6b6622132db5d75a2fbe
Liam-Weitzel <liam.weitzel@gmail.com>
2021-11-30 11:28:29 +0000

    Feature: Show local database results on index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ID, FirstName, LastName FROM exampletable";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["ID"]. " - Name: " . $row["FirstName"]. " " . $row["LastName"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+?>
